3047de5405fabc41a158bdb1f4a736206d210ca3
studiobrain <josh@studiobrainchild.com>
2019-03-02 20:27:13 -0600

    updates to the fetch calls and the UI added

--- App.js
+++ App.js
@@ -1,5 +1,13 @@
 import React, {Component, PropTypes} from 'react'
-import {Platform, StyleSheet, Text, View} from 'react-native'
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  FlatList,
+  Dimensions
+} from 'react-native'
 import {observer} from 'mobx-react'
 import {extendObservable} from 'mobx'
 
@@ -10,34 +18,74 @@ const App = observer(class App extends Component {
     super(props)
 
     extendObservable(this, {
-      data: undefined,
+      people: undefined,
+      profile: undefined,
       error: undefined
     })
   }
 
-  componentDidMount() {
-    console.log('well')
-    fetch(`/people`, {
+  fetchPeople = () => {
+    fetch('http://localhost:8080/people', {
       method: 'GET',
       headers: {
         'Content-Type': 'application/json',
         'Accept': 'application/json'
       }
     }).then(res => {
-      console.log(res)
       return res.json()
     }).then(response => {
-      console.log(response)
-      this.data = response
+      this.people = response.people
+      this.profile = undefined
     }).catch(error => {
       this.error = error.message || error.error
     })
   }
 
+  fetchPerson = (id) => {
+    fetch(`http://localhost:8080/people/${id}`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      }
+    }).then(res => {
+      return res.json()
+    }).then(response => {
+      this.profile = response.profile
+      this.people = undefined
+    }).catch(error => {
+      this.error = error.message || error.error
+    })
+  }
+
+  rederListItem = ({item}) => {
+    return (
+      <TouchableOpacity onPress={() => this.fetchPerson(item.id)}>
+        <View style={styles.peopleListItem}>
+          <Text>{item.name}</Text>
+        </View>
+      </TouchableOpacity>
+    )
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>Im Here!</Text>
+        <Text style={styles.header}>Flyreel Swapi Wars</Text>
+        <Text style={styles.subHeader}>React Developer Test</Text>
+        {!!this.error && <Text>{this.error}</Text>}
+        <TouchableOpacity onPress={() => this.fetchPeople()}>
+          <View style={styles.button}>
+            <Text style={styles.buttonText}>Fetch People</Text>
+          </View>
+        </TouchableOpacity>
+        {!!this.people && <FlatList style={styles.peopleList} data={this.people} renderItem={this.rederListItem}/>}
+        {!!this.profile && <View style={styles.profile}>
+          <Text style={styles.profileHeaderText}>{this.profile.name}</Text>
+          <Text style={styles.profileText}>birthday: {this.profile.birth_year}</Text>
+          <Text style={styles.profileText}>created: {this.profile.created}</Text>
+          <Text style={styles.profileText}>edited: {this.profile.edited}</Text>
+        </View>}
       </View>
     )
   }
@@ -46,19 +94,58 @@ const App = observer(class App extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    alignItems: 'center',
+    backgroundColor: 'white',
+  },
+  header: {
+    marginTop: 150,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    fontWeight: 'bold',
+    fontSize: 28,
   },
-  welcome: {
+  subHeader: {
+    fontSize: 16,
+    color: 'rgba(0,0,0,0.5)',
+    marginBottom: 24,
+  },
+  button: {
+    height: 50,
+    width: Dimensions.get('window').width * 0.9,
+    backgroundColor: 'green',
+    borderRadius: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 10,
+    marginBottom: 10,
+  },
+  buttonText: {
+    color: 'white',
+  },
+  peopleListItem: {
+    height: 50,
+    width: Dimensions.get('window').width * 0.9,
+    backgroundColor: 'rgba(0,0,0,0.15)',
+    borderRadius: 4,
+    borderColor: 'rgba(0,0,0,0.25)',
+    borderWidth: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 4,
+    marginBottom: 4,
+  },
+  profile: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  profileHeaderText: {
+    fontWeight: 'bold',
     fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+    marginBottom: 10,
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  profileText: {
+    fontSize: 14,
+    color: 'rgba(0,0,0,0.5)',
   },
 })
 
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const path = require('path')
+const fetch = require('node-fetch')
 const app = express()
 const cors = require('cors')
 const port = process.env.PORT || 8080
@@ -7,41 +8,49 @@ const port = process.env.PORT || 8080
 app.use(cors())
 
 app.get('/api', (req, res) => {
+  console.log('/api')
   res.send({code: 200, message: 'I have arrived!'})
 })
 
-app.get('api/people', (req, res) => {
-  const people = fetch('https://swapi.co/api/people', {
+app.get('/people', (req, res) => {
+  fetch('https://swapi.co/api/people', {
     method: 'GET',
     headers: {
       'Content-Type': 'application/json',
       'Accept': 'application/json'
     }
   }).then(res => {
-    console.log(res)
+    if (res.status !== 200) {
+      res.send({code: res.status, message: 'Apparently you did something wrong!'})
+      return
+    }
     return res.json()
   }).then(response => {
-    console.log(response)
-    return response
+    let peopleList = response.results.slice(0, 5)
+    peopleList.map((item, i) => {
+      return item['id'] = i + 1
+    })
+    res.send({code: 200, message: 'success', people: peopleList})
   }).catch(error => {
-    this.error = error.message || error.error
+    console.log('error:', error)
   })
-  console.log(people)
 })
 
-app.get('api/people/:id', (req, res) => {
-  fetch(`https://swapi.co/api/people:${id}`, {
+app.get('/people/:id', (req, res) => {
+  fetch(`https://swapi.co/api/people/${req.params.id}`, {
     method: 'GET',
     headers: {
       'Content-Type': 'application/json',
       'Accept': 'application/json'
     }
   }).then(res => {
-    console.log(res)
+    if (res.status !== 200) {
+      res.send({code: res.status, message: 'Apparently you did something wrong!'})
+      return
+    }
     return res.json()
   }).then(response => {
-    console.log(response)
-    return response
+    res.send({code: 200, message: 'success', profile: response})
   }).catch(error => {
     this.error = error.message || error.error
   })
